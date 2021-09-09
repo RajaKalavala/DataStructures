ad6f987cd89e3054c9ec60b8791bb26f5d316145
K. Raja <45046033+RajaKalavala@users.noreply.github.com>
2021-09-09 09:02:28 +0000

    BigO - findingNemo

--- BigO/findingNemo.ts
+++ BigO/findingNemo.ts
@@ -1,1 +1,11 @@
-console.log("findingNemo code")
+const nemo = ["nemo"];
+
+function findNemo(array : any) {
+  for(let i=0; i<array.length; i++){
+    if(array[i] === "nemo"){
+      console.log('Found Nemo');
+    }
+  }
+}
+
+findNemo(nemo);
